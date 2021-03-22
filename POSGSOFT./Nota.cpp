98142822539fe8d76073a075fd1134534e5018ca
JohannR513 <57722211+JohannR513@users.noreply.github.com>
2021-03-22 16:23:44 -0500

    Add files via upload

---
+++ POSGSOFT./Nota.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Nota.h"
+
+
+Nota::Nota (){
+}
+
+
+Nota::Nota(Criterio criterio, string observacion1, string observacion2, double notaJuradoUno, double notaJur2){
+	this->criterio = criterio;
+	this->observacion1 = observacion1;
+	this->observacion2 = observacion2;
+	this->notaJuradoUno = notaJuradoUno;
+	this->notaJuradoDos = notaJuradoDos;
+}
+
+
+void Nota::crearNota(int idNota){
+	criterio.crearCriterio( int idCriterio );
+
+	cout << " Digite la nota 1: " << endl;
+	cin >> this->notaJuradoUno;
+
+	cout << " Digite la nota 2: " << endl;
+	cin >> this->notaJuradoDos;
+
+	cout << " Comentario del jurado 1: " << endl;
+	fflush(stdin);
+	getline(cin, this->observacion1);  
+	fflush(stdin);
+
+	cout << " Comentario del jurado 2: " << endl;
+	fflush(stdin);
+	getline(cin, observacion2);  
+	fflush(stdin);
+}
+
+void Nota::mostrarCalificacion(){
+	criterio.mostrarCriterio(); 
+	cout << " Nota Jurado 1: " << notaJuradoUno << endl;
+	cout << " Nota Jurado 2: " << notaJuradoDos << endl;
+	cout << " Comentario sobre las notas: " << observacion1 << endl;
+}
+
+
+double Nota::getNotaJuradoUno(){
+	return notaJuradoUno;
+}
+
+
+double Nota::getNotaJuradoDos(){
+	return notaJuradoDos;
+}
+
+
+double Nota::getPorcentajeNotaCriterio(){
+	return criterio.getPorcentajeNota();
+}
---
+++ POSGSOFT./Nota.h
@@ -0,0 +1,34 @@
+#ifndef NOTA_H
+#define NOTA_H
+
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include "Criterio.h"
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+class Nota{
+
+private:
+	Criterio criterio;
+	string observacion1, observacion2;
+	double notaJuradoUno;
+	double notaJuradoDos;
+	
+
+public:
+	Nota();  
+	Nota(Criterio, string, string, double, double);  
+	void crearNota( int idNota );  
+	void mostrarCalificacion(); 
+	double getNotaJuradoUno();  
+	double getNotaJuradoDos();  
+	double getPorcentajeNotaCriterio(); 
+};
+
+#endif
