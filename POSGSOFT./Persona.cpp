212d3a149dfb52111ce838f598b35e494f2a2b72
JohannR513 <57722211+JohannR513@users.noreply.github.com>
2021-03-22 16:49:20 -0500

    Update Persona.cpp

--- POSGSOFT./Persona.cpp
+++ POSGSOFT./Persona.cpp
@@ -44,39 +44,64 @@ void Persona::crearPersona( int opcion1 ){
 	fflush( stdin );
  	
  	
-	if(opcion1 == 1){
+	if( opcion1 == 1 ){
 		rolPersona = "Director";
 	}
-	else if(opcion1 == 2){
+	else if( opcion1 == 2 ){
 		rolPersona = "Codirector";
 	}
-	else if(opcion1 == 3){
+	else if(opcion1 == 3 ){
 		rolPersona = "Estudiante";
 	}
-	else if(opcion1 == 4){
-		while(salida == 1){
+	else if( opcion1 == 4 ){
+		while( salida == 1 ){
 			cout << " Digite: \n 1. Si es jurado interno\n 2. Si es jurado externo\n" << endl;  
 			cin >> opcionJurado;
-			if(opcionJurado < 1){
+			if( opcionJurado < 1 ){
 				cout << " Error!, esta no es una opcion valida" << endl; 
 				salida == 1;
 			}
-			else if(opcionJurado == 1){
+			else if( opcionJurado == 1 ){
 				rolPersona = "Jurado Interno";
 				salida = 0;
 			}
-			else if(opcionJurado == 2){
+			else if( opcionJurado == 2 ){
 				rolPersona = "Jurado Externo";
 				salida = 0;
 			}
-			else if(opcionJurado > 2){
+			else if( opcionJurado > 2){
 				cout << " Error!, esta no es una opcion valida" << endl;  
 		}
 	}
 }
-
+}
 void Persona::mostrarPersona(){
-	cout << "Nombres: " << this->nombres << endl;
-	cout << "Apellidos: " << this->apellidos << endl;
-	cout << "Rol: " << this->rolPersona << endl;
+	cout << "Nombres: " << this-> nombre << endl;
+	cout << "Apellidos: " << this->apellido << endl;
+	cout << "Rol: " << this-> rol << endl;
+ }
+ string Persona::getNombre(){
+    return nombre;
+}
+
+void Persona::setApellidos( string apellidos ){
+    this->apellido = apellido;
+    return;
+}
+
+string Persona::getApellido(){
+    return apellido;
+}
+
+void Persona::setRolPersona( rolPersona rol ){
+    this->rol = rol;
+    return;
+}
+
+int Persona::getRolPersona(){
+    return rol;
+} 
+
+int Persona::getIdPersona(){
+	return id;
 }
