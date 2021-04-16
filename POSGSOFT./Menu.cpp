d88ff8e36ec2a600e729d40cc071254345ef1c24
JohannR513 <57722211+JohannR513@users.noreply.github.com>
2021-04-16 10:32:15 -0500

    Update Menu.cpp

--- POSGSOFT./Menu.cpp
+++ POSGSOFT./Menu.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-#include "menu.h"
+#include "Menu.h"
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
 
 int menu()
 {
@@ -8,13 +13,29 @@ int menu()
     std::cout << "Menu Principal"<< std::endl;
     std::cout << "Por favor, elige una opcion:\n "<< std::endl;
     
-    std::cout << "1. OPC 1" << std::endl;
-    std::cout << "2. OPC 2" << std::endl;
-    std::cout << "3. OPC 3" << std::endl;
-    std::cout << "4. OPC 4" << std::endl;
-    std::cout << "5. OPC 5" << std::endl;
-    std::cout << "0. Salir\n"<< std::endl;
+    cout << "1. Crear un acta" << endl;
+    cout << "2. Modificar un acta" << endl;
+    cout << "3. Cerrar acta y mostrar txt " << endl;
+    cout << "4. Eliminar acta" << endl;
+    cout << "5. Desplegar menu de otras funcionalidades" << endl;
+    cout << "0. Salir" << endl;
     
     std::cin >> opcion;
     return opcion;
 } 
+int menu2(){
+	int opcion2;
+
+	cout << "1. Cuantas actas existen de tipo aplicado  " << endl;
+	cout << "2. Cuantas actas existen de tipo  investigacion " << endl;
+	cout << "3. Consultar cuantos trabajo han sido dirigidos por un profesor " << endl;
+	cout << "4. Consultar en cuales actas ha sido jurado un profesor " << endl;
+	cout << "5. Ver la lista de jurados que han trabajo en las actas  " << endl;
+	cout << "6. Consultar las actas que estan en pendiente o rechazadas " << endl;
+	cout << "7. Consultar cuales jurados son internos y cuales son externos " << endl;
+	cout << "8. Consultar la lista de criterios de un acta " << endl;
+ 	cout << "0. Volver al menu anterior " << endl;
+	cin >> opcion2;
+	
+  return opcion2;
+}
